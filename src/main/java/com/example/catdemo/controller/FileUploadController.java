20a2de2e8056649da91cd660e56a1c6b5faa3571
zwx487200 <1735927267@qq.com>
2024-09-29 20:29:41 +0800

    文件上传

--- src/main/java/com/example/catdemo/controller/FileUploadController.java
+++ src/main/java/com/example/catdemo/controller/FileUploadController.java
@@ -2,11 +2,13 @@ package com.example.catdemo.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
-import java.io.IOException;
+import java.util.Objects;
+import java.util.UUID;
 
 /**
  * @author jennyzhuzhu
@@ -21,13 +23,19 @@ public class FileUploadController {
     @PostMapping("/upload")
     public ResponseEntity<String> handleFileUpload(@RequestParam("file") MultipartFile file) {
         try {
-            // 这里可以添加你的文件保存逻辑
-            byte[] bytes = file.getBytes();
+            // 判断文件夹是否存在，没有则新增
+            File dir = new File("D:\\wenjianshangchuandemo");
+            if (!dir.exists()) {
+                dir.mkdirs();
+            }
+            // 将文件名修改为UUID()生成的
+            String fileName = StringUtils.cleanPath(Objects.requireNonNull(file.getOriginalFilename()));
+            String originFileName = UUID.randomUUID().toString() + "." + StringUtils.getFilenameExtension(fileName);;
             //把文件的内容存储到本地磁盘上
-            String originFileName = file.getOriginalFilename();
+            //String originFileName = file.getOriginalFilename();
             file.transferTo(new File("D:\\wenjianshangchuandemo\\"+originFileName));
 
-            return ResponseEntity.ok("文件上传成功：" + file.getOriginalFilename());
+            return ResponseEntity.ok(originFileName);
         } catch (Exception e) {
             return ResponseEntity.internalServerError().body("文件上传失败：" + e.getMessage());
         }
